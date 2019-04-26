cf010ddc3442d3b4c362b23714c47a201cb15b57
Alexandra Romanova <alexandraromanova@Alexandras-MacBook-Air.local>
2019-04-26 15:45:41 -0400

    show pageadded, some css, new route

--- controllers/destenation_controller.js
+++ controllers/destenation_controller.js
@@ -14,22 +14,41 @@ router.get('/trips', (req, res) => {
         });
     })
 })
+//INDEX route for client list trips.
+router.get('/newlist', (req, res) => {
+    travel.find({}, (error, newTrips)=>{
+        res.render('newlist.ejs', {
+            trip: newTrips
+        });
+    })
+})
 
-//** */ NEW (CLIENT create)http://localhost:3000/desteny/new
+//** */ NEW (CLIENT create)
+//take to the page : http://localhost:3000/desteny/new
 router.get('/new', (req, res) => {
     res.render('new.ejs')
 })
 // CREATE (SERVER)
-router.post('/', (req, res) => {
-    
+router.post('/newlist', (req, res) => {
+    //created new travel(model) in db.
     travel.create(req.body, (error, createdTravel)=>{
+        console.log(req.body)
         if (error) {
             res.send(error)
         } else {
-            res.redirect('/trips');
+            res.redirect('/desteny/newlist');
         }
     });
 })
 
+//******* SHOW ROUTE */
+router.get('/desteny/show/:id', (req,res) => {
+    travel.findById(req.params.id, (err, showTrip)=> {
+        res.render('show.ejs', {
+            trip: showTrip
+        })
+    })
+    })
+    
 
 module.exports = router
